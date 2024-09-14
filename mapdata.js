f9905d5590b2fe4a8361b86935e400b91aae1e20
SoeurnRotha_Blue <rotha_soeurn@blue.com.kh>
2024-09-14 16:10:35 +0700

    first

---
+++ mapdata.js
@@ -0,0 +1,363 @@
+var simplemaps_countrymap_mapdata = {
+  main_settings: {
+    //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect"
+  },
+  state_specific: {
+    KH1: {
+      name: "Banteay Meanchey",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH10: {
+      name: "Kratie",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH11: {
+      name: "Mondul Kiri",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH12: {
+      name: "Phnom Penh",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH13: {
+      name: "Preah Vihear",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH14: {
+      name: "Prey Veng",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH15: {
+      name: "Pursat",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH16: {
+      name: "Ratanak Kiri",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH17: {
+      name: "Siemreap",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH18: {
+      name: "Preah Sihanouk",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH19: {
+      name: "Stung Treng",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH2: {
+      name: "Battambang",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH20: {
+      name: "Svay Rieng",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH21: {
+      name: "Takeo",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH22: {
+      name: "Oddar Meanchey",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH23: {
+      name: "Kep",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH24: {
+      name: "Pailin",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH25: {
+      name: "Tboung Khmum",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH3: {
+      name: "Kampong Cham",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH4: {
+      name: "Kampong Chhnang",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH5: {
+      name: "Kampong Speu",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH6: {
+      name: "Kampong Thom",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH7: {
+      name: "Kampot",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH8: {
+      name: "Kandal",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    KH9: {
+      name: "Koh Kong",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    }
+  },
+  locations: {
+    "0": {
+      name: "Phnom Penh",
+      lat: "11.56245",
+      lng: "104.916006"
+    }
+  },
+  labels: {
+    KH1: {
+      name: "Banteay Meanchey",
+      parent_id: "KH1"
+    },
+    KH10: {
+      name: "Kratie",
+      parent_id: "KH10"
+    },
+    KH11: {
+      name: "Mondul Kiri",
+      parent_id: "KH11"
+    },
+    KH12: {
+      name: "Phnom Penh",
+      parent_id: "KH12"
+    },
+    KH13: {
+      name: "Preah Vihear",
+      parent_id: "KH13"
+    },
+    KH14: {
+      name: "Prey Veng",
+      parent_id: "KH14"
+    },
+    KH15: {
+      name: "Pursat",
+      parent_id: "KH15"
+    },
+    KH16: {
+      name: "Ratanak Kiri",
+      parent_id: "KH16"
+    },
+    KH17: {
+      name: "Siemreap",
+      parent_id: "KH17"
+    },
+    KH18: {
+      name: "Preah Sihanouk",
+      parent_id: "KH18"
+    },
+    KH19: {
+      name: "Stung Treng",
+      parent_id: "KH19"
+    },
+    KH2: {
+      name: "Battambang",
+      parent_id: "KH2"
+    },
+    KH20: {
+      name: "Svay Rieng",
+      parent_id: "KH20"
+    },
+    KH21: {
+      name: "Takeo",
+      parent_id: "KH21"
+    },
+    KH22: {
+      name: "Oddar Meanchey",
+      parent_id: "KH22"
+    },
+    KH23: {
+      name: "Kep",
+      parent_id: "KH23"
+    },
+    KH24: {
+      name: "Pailin",
+      parent_id: "KH24"
+    },
+    KH25: {
+      name: "Tboung Khmum",
+      parent_id: "KH25"
+    },
+    KH3: {
+      name: "Kampong Cham",
+      parent_id: "KH3"
+    },
+    KH4: {
+      name: "Kampong Chhnang",
+      parent_id: "KH4"
+    },
+    KH5: {
+      name: "Kampong Speu",
+      parent_id: "KH5"
+    },
+    KH6: {
+      name: "Kampong Thom",
+      parent_id: "KH6"
+    },
+    KH7: {
+      name: "Kampot",
+      parent_id: "KH7"
+    },
+    KH8: {
+      name: "Kandal",
+      parent_id: "KH8"
+    },
+    KH9: {
+      name: "Koh Kong",
+      parent_id: "KH9"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
